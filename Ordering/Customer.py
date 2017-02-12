b0ef82d966a32ce31a398c09234866d181335a30
kkleiven <kkleiven@andrew.cmu.edu>
2017-02-12 12:31:55 -0500

    Order Objects
    Created order and customer objects to be used to track info about each
    order/customer

---
+++ Ordering/Customer.py
@@ -0,0 +1,14 @@
+class Customer(object):
+
+    def __init__(self, id):
+        self.id = id
+
+    def getID(self):
+        return self.id
+
+    def getPosition(self):
+        #use id to get wristband data
+        raise "Not yet implemented"
+
+    def __repr__(self):
+        return "Customer %d" % self.id
---
+++ Ordering/Order.py
@@ -0,0 +1,29 @@
+class Order(object):
+    orderID = 0 # so we can assign a unique id to each order
+
+    def __init__(self, customer=None, orderType=None):
+        self.id = Order.orderID
+        Order.orderID += 1
+        self.customer = customer
+        self.orderType = orderType
+
+    def getID(self):
+        return self.id
+
+    def setCustomer(self, customer):
+        self.customer = customer
+
+    def getCustomer(self):
+        return self.customer
+
+    def setOrderType(self, orderType):
+        self.orderType = orderType
+
+    def getOrderType(self):
+        return self.orderType
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __repr__(self):
+        return "Order %d: %r %s" % (self.id, self.customer, self.orderType)
